15b5131d4c5085091b573a0cd383d5a946a54a69
NoelHuereca <huereca112@gmail.com>
2023-03-26 15:20:13 -0600

    Se añadieron funciones nuevas, ahora la aplicacion puedea añadir, mostrar, editar y eliminar informacion

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,19 +1,114 @@
-/* funciones */
-function agregarEmpleado(){
-    /* elementos */
-    const nombre = document.getElementById("nombre")
-    const apellido = document.getElementById("apellido")
-    const edad = document.getElementById("edad")
-    const puesto = document.getElementById("puesto")
-    const salario = document.getElementById("salario")
+/* elementos */
+const idEmpleado = document.getElementById("id")
+const nombre = document.getElementById("nombre")
+const apellido = document.getElementById("apellido")
+const edad = document.getElementById("edad")
+const puesto = document.getElementById("puesto")
+const salario = document.getElementById("salario")
+const empleados = JSON.parse(localStorage.getItem("empleados")) || []
 
-    /* agregar al Local Storage */
-    localStorage.setItem("nombre", nombre.value)
-    localStorage.setItem("apellido", apellido.value)
-    localStorage.setItem("edad", edad.value)
-    localStorage.setItem("puesto", puesto.value)
-    localStorage.setItem("salario", salario.value)
+const bodyTable = document.getElementById("bodyTable")
+
+const btnAgregar = document.getElementById("btnAgregar")
+const btnConfirmar = document.getElementById("btnConfirmar")
+/* Arreglo de objeto de empleados y objeto empleado*/
+
+/* funciones */
+const agregarEmpleado = () =>{
+    let empleado = { //crea un nuevo objeto empleado
+        id: crypto.randomUUID(), //le asigna un id unico
+        nombre: nombre.value,
+        apellido: apellido.value,
+        edad: edad.value,
+        puesto: puesto.value,
+        salario: salario.value
+    }
+
+    empleados.push(empleado) //agrega el objeto al arreglo empleados
+    console.log(empleados) //imprime en consola los cambios del arreglo
+    localStorage.setItem("empleados",JSON.stringify(empleados))
+
+    nombre.value=""
+    apellido.value=""
+    edad.value=""
+    puesto.value="default"
+    salario.value=""
+
+    mostrarEmpleado()
 }
+
+const mostrarEmpleado = () =>{
+    bodyTable.innerHTML = ""
+    empleados.forEach((empleado) => {
+        bodyTable.innerHTML += `<tr>
+        <th scope="row">${empleado.id}</th>
+        <td>${empleado.nombre}</td>
+        <td>${empleado.apellido}</td>
+        <td>${empleado.edad}</td>
+        <td>${empleado.puesto}</td>
+        <td>${empleado.salario}</td>
+        <td><button onclick="eliminarEmpleado('${empleado.id}')">Eliminar</button></td>
+        <td><button onclick="editarEmpleado('${empleado.id}')">Editar</button></td>
+        </tr>
+        `
+    });
+}
+
+const eliminarEmpleado = (id) =>{
+    const empleado = empleados.find((empleado) => empleado.id === id)
+    console.log(empleado,"Eliminado")
+    const index = empleados.indexOf(empleado)
+    empleados.splice(index,1)
+    localStorage.setItem("empleados",JSON.stringify(empleados))
+    mostrarEmpleado()
+}
+
+const editarEmpleado = (id) =>{
+    const empleado = empleados.find((empleado) => empleado.id === id)
+    console.log(empleado,"editando")
+    nombre.value = empleado.nombre
+    apellido.value = empleado.apellido
+    edad.value = empleado.edad
+    puesto.value = empleado.puesto
+    salario.value = empleado.salario
+    idEmpleado.style.display = "inline"
+    idEmpleado.value = empleado.id
+    btnAgregar.style.display = "none"
+    btnConfirmar.style.display = "inline"
+}
+
+const confirmarEdicion = () =>{
+    const empleado = empleados.find((empleado)=> empleado.id === id.value)
+    empleado.nombre = nombre.value
+    empleado.apellido = apellido.value
+    empleado.edad = edad.value
+    empleado.puesto = puesto.value
+    empleado.salario = salario.value
+    localStorage.setItem("empleados", JSON.stringify(empleados))
+    btnAgregar.style.display = "inline"
+    btnConfirmar.style.display = "none"
+    id.style.display = "none"
+    nombre.value = ""
+    apellido.value = ""
+    edad.value = ""
+    puesto.value = "default"
+    salario.value = ""
+    mostrarEmpleado()
+}
+
+btnAgregar.addEventListener("click",(Event)=>{
+    Event.preventDefault()
+    agregarEmpleado()
+})
+btnConfirmar.addEventListener("click",(Event)=>{
+    Event.preventDefault()
+    confirmarEdicion()
+})
+
+
+
+
+
 
 /* checkbox */
 function alternar(checkbox){
@@ -24,3 +119,6 @@ function alternar(checkbox){
         document.querySelector('input[name="masculino"]').checked = false
     }
 }
+
+/* mostrar empleados cada que cargue la pagina */
+window.addEventListener("load",mostrarEmpleado)
